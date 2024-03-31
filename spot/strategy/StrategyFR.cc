0bde972656706695d3b8365884fcada5916ad348
truistin <truistin@gmail.com>
2024-04-01 00:59:58 +0800

    fix code

--- spot/strategy/StrategyFR.cc
+++ spot/strategy/StrategyFR.cc
@@ -1171,7 +1171,7 @@ void StrategyFR::OnRtnInnerMarketDataTradingLogic(const InnerMarketData &marketD
     int64_t ts = CURR_MSTIME_POINT;
     LOG_INFO << "market data beyond time: " << marketData.InstrumentID << ", EpochTime: " << marketData.EpochTime
             << ", ts: " << ts << ", marketData.EpochTime - ts: " << marketData.EpochTime - ts;
-    if (ts - marketData.EpochTime > 300) {
+    if (marketData.EpochTime - ts > 300) {
         LOG_WARN << "market data beyond time: " << marketData.InstrumentID << ", EpochTime: " << marketData.EpochTime
             << ", ts: " << ts << ", gap: " << marketData.EpochTime - ts;
         return;
